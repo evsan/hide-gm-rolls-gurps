48487c0742a95d8b79ba16e01497c1822b55ed07
Andrei Evseenko <a.yevseyenko@gmail.com>
2024-01-28 01:38:45 +0000

    v0.18.0

--- modules/hide_gm_rolls.js
+++ modules/hide_gm_rolls.js
@@ -150,7 +150,7 @@ class HideGMRolls {
 		const rollMessage = html.find('div.roll-message');
 		const rollValue = html.find('div.roll-message .roll-value');
 		const rollDetails = html.find('div.roll-message .roll-result div:nth-child(2) .aside');
-		if (rollMessage && rollValue && rollDetails) {
+		if (!!rollMessage && !!rollValue && !!rollDetails) {
 			rollMessage.html('<div class="roll-result"><div><span class="roll-value">'+rollValue.html()+'</span></div><div><hr><div class="aside">' + rollDetails.html() + '</div></div></div>');
 		}
 	}
